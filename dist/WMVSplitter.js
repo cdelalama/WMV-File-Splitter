3a93bcc7653c7b5c97f46bafb5da6dc7928f1b3b
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-09-02 00:28:24 +0200

    Fix Incorrect Time Measurement in Video Splitting Task
    This commit addresses the issue where the time measurement for chunking and processing WMV video files was not accurately reflected in the logs. By wrapping the splitWMV function's ffmpeg processes within a promise, we ensure that completion times are logged only after all processes have finished. This adjustment provides a more accurate measure of total processing time. TypeScript strict mode compatibility has also been improved by specifying that the promise resolves without a value.

--- dist/WMVSplitter.js
+++ dist/WMVSplitter.js
@@ -7,72 +7,82 @@ import * as path from "path";
 import ffmpeg from "fluent-ffmpeg";
 import ora from "ora";
 const splitWMV = (inputPath, outputPath, chunkSize) => {
-    const originalFileName = path.basename(inputPath, path.extname(inputPath));
-    const processedPath = path.join(__dirname, "../processed");
-    if (!fs.existsSync(outputPath)) {
-        fs.mkdirSync(outputPath);
-    }
-    if (!fs.existsSync(processedPath)) {
-        fs.mkdirSync(processedPath);
-    }
-    console.log(`Processing file at ${inputPath}`);
-    ffmpeg.ffprobe(inputPath, function (err, metadata) {
-        if (err || !metadata.format || metadata.format.duration === undefined) {
-            console.error("Could not retrieve video duration.");
-            return;
+    return new Promise((resolve, reject) => {
+        const originalFileName = path.basename(inputPath, path.extname(inputPath));
+        const processedPath = path.join(__dirname, "../processed");
+        if (!fs.existsSync(outputPath)) {
+            fs.mkdirSync(outputPath);
         }
-        const duration = metadata.format.duration;
-        const fileSize = fs.statSync(inputPath).size;
-        const numChunks = Math.ceil(fileSize / chunkSize);
-        const chunkDuration = duration / numChunks;
-        const spinner = ora(`Processing ${numChunks} chunks of approximately ${chunkSize / (1024 * 1024)} MB. This may take a while...`).start();
-        let completedChunks = 0;
-        for (let i = 0; i < numChunks; i++) {
-            const start = i * chunkDuration;
-            const outputFileName = `${originalFileName}-chunk-${i}.wmv`;
-            const outputFilePath = path.join(outputPath, outputFileName);
-            ffmpeg(inputPath)
-                .setStartTime(start)
-                .setDuration(chunkDuration)
-                .output(outputFilePath)
-                .on("end", function (err) {
-                completedChunks++;
-                if (completedChunks === numChunks) {
-                    spinner.stop();
-                    const newLocation = path.join(processedPath, path.basename(inputPath));
-                    fs.rename(inputPath, newLocation, (err) => {
-                        if (err)
-                            console.error(`Error moving file: ${err}`);
-                        console.log(`Moved input file to ${newLocation}`);
-                    });
-                }
-            })
-                .on("error", function (err) {
-                console.log("error: ", err);
-            })
-                .run();
+        if (!fs.existsSync(processedPath)) {
+            fs.mkdirSync(processedPath);
         }
+        console.log(`Processing file at ${inputPath}`);
+        ffmpeg.ffprobe(inputPath, function (err, metadata) {
+            if (err || !metadata.format || metadata.format.duration === undefined) {
+                console.error("Could not retrieve video duration.");
+                return;
+            }
+            const duration = metadata.format.duration;
+            const fileSize = fs.statSync(inputPath).size;
+            const numChunks = Math.ceil(fileSize / chunkSize);
+            const chunkDuration = duration / numChunks;
+            const spinner = ora(`Processing ${numChunks} chunks of approximately ${chunkSize / (1024 * 1024)} MB. This may take a while...`).start();
+            let completedChunks = 0;
+            for (let i = 0; i < numChunks; i++) {
+                const start = i * chunkDuration;
+                const outputFileName = `${originalFileName}-chunk-${i}.wmv`;
+                const outputFilePath = path.join(outputPath, outputFileName);
+                ffmpeg(inputPath)
+                    .setStartTime(start)
+                    .setDuration(chunkDuration)
+                    .output(outputFilePath)
+                    .on("end", function (err) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    completedChunks++;
+                    if (completedChunks === numChunks) {
+                        spinner.stop();
+                        const newLocation = path.join(processedPath, path.basename(inputPath));
+                        fs.rename(inputPath, newLocation, (err) => {
+                            if (err) {
+                                console.error(`Error moving file: ${err}`);
+                                reject(err);
+                                return;
+                            }
+                            console.log(`Moved input file to ${newLocation}`);
+                            resolve();
+                        });
+                    }
+                })
+                    .on("error", function (err) {
+                    console.log("error: ", err);
+                    reject(err);
+                })
+                    .run();
+            }
+        });
     });
 };
 const inputDir = path.join(__dirname, "../input/");
 const outputPath = path.join(__dirname, "../output/");
 const chunkSize = 100 * 1024 * 1024; // 100MB
-// Find all .wmv files in the input directory
 const files = fs.readdirSync(inputDir).filter((file) => file.endsWith(".wmv"));
-// If no .wmv files found in the input directory, warn and end the script
 if (files.length === 0) {
     console.warn("No .wmv files found in the input directory. Terminating script.");
     process.exit(0);
 }
-// Start timer
 const startTime = Date.now();
-// Process each file
-files.forEach((file) => {
+const processingTasks = files.map((file) => {
     const inputPath = path.join(inputDir, file);
-    splitWMV(inputPath, outputPath, chunkSize);
+    return splitWMV(inputPath, outputPath, chunkSize);
 });
-// End the timer and log the duration
-const endTime = Date.now();
-const elapsedTime = (endTime - startTime) / 1000;
-console.log(`Processing completed in ${elapsedTime} seconds.`);
+Promise.all(processingTasks).then(() => {
+    const endTime = Date.now();
+    const elapsedTime = (endTime - startTime) / 1000;
+    console.log(`Processing completed in ${elapsedTime.toFixed(2)} seconds.`);
+}).catch((error) => {
+    console.error("Error during process: ", error);
+});
 //# sourceMappingURL=WMVSplitter.js.map
--- src/WMVSplitter.ts
+++ src/WMVSplitter.ts
@@ -10,91 +10,103 @@ import ffmpeg from "fluent-ffmpeg";
 import ora from "ora";
 
 const splitWMV = (inputPath: string, outputPath: string, chunkSize: number) => {
-	const originalFileName = path.basename(inputPath, path.extname(inputPath));
-	const processedPath = path.join(__dirname, "../processed");
+    return new Promise<void>((resolve, reject) => {
+        const originalFileName = path.basename(inputPath, path.extname(inputPath));
+        const processedPath = path.join(__dirname, "../processed");
 
-	if (!fs.existsSync(outputPath)) {
-		fs.mkdirSync(outputPath);
-	}
-	if (!fs.existsSync(processedPath)) {
-		fs.mkdirSync(processedPath);
-	}
+        if (!fs.existsSync(outputPath)) {
+            fs.mkdirSync(outputPath);
+        }
+        if (!fs.existsSync(processedPath)) {
+            fs.mkdirSync(processedPath);
+        }
 
-	console.log(`Processing file at ${inputPath}`);
+        console.log(`Processing file at ${inputPath}`);
 
-	ffmpeg.ffprobe(inputPath, function (err, metadata) {
-		if (err || !metadata.format || metadata.format.duration === undefined) {
-			console.error("Could not retrieve video duration.");
-			return;
-		}
+        ffmpeg.ffprobe(inputPath, function (err, metadata) {
+            if (err || !metadata.format || metadata.format.duration === undefined) {
+                console.error("Could not retrieve video duration.");
+                return;
+            }
 
-		const duration: number = metadata.format.duration;
-		const fileSize: number = fs.statSync(inputPath).size;
-		const numChunks: number = Math.ceil(fileSize / chunkSize);
-		const chunkDuration: number = duration / numChunks;
+            const duration: number = metadata.format.duration;
+            const fileSize: number = fs.statSync(inputPath).size;
+            const numChunks: number = Math.ceil(fileSize / chunkSize);
+            const chunkDuration: number = duration / numChunks;
 
-		const spinner = ora(
-			`Processing ${numChunks} chunks of approximately ${
-				chunkSize / (1024 * 1024)
-			} MB. This may take a while...`
-		).start();
+            const spinner = ora(
+                `Processing ${numChunks} chunks of approximately ${
+                    chunkSize / (1024 * 1024)
+                } MB. This may take a while...`
+            ).start();
 
-		let completedChunks = 0;
+            let completedChunks = 0;
 
-		for (let i = 0; i < numChunks; i++) {
-			const start = i * chunkDuration;
-			const outputFileName = `${originalFileName}-chunk-${i}.wmv`;
-			const outputFilePath = path.join(outputPath, outputFileName);
+            for (let i = 0; i < numChunks; i++) {
+                const start = i * chunkDuration;
+                const outputFileName = `${originalFileName}-chunk-${i}.wmv`;
+                const outputFilePath = path.join(outputPath, outputFileName);
 
-			ffmpeg(inputPath)
-				.setStartTime(start)
-				.setDuration(chunkDuration)
-				.output(outputFilePath)
-				.on("end", function (err) {
-					completedChunks++;
-					if (completedChunks === numChunks) {
-						spinner.stop();
-						const newLocation = path.join(
-							processedPath,
-							path.basename(inputPath)
-						);
-						fs.rename(inputPath, newLocation, (err) => {
-							if (err) console.error(`Error moving file: ${err}`);
-							console.log(`Moved input file to ${newLocation}`);
-						});
-					}
-				})
-				.on("error", function (err) {
-					console.log("error: ", err);
-				})
-				.run();
-		}
-	});
+                ffmpeg(inputPath)
+                    .setStartTime(start)
+                    .setDuration(chunkDuration)
+                    .output(outputFilePath)
+                    .on("end", function (err) {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+
+                        completedChunks++;
+                        if (completedChunks === numChunks) {
+                            spinner.stop();
+                            const newLocation = path.join(
+                                processedPath,
+                                path.basename(inputPath)
+                            );
+                            fs.rename(inputPath, newLocation, (err) => {
+                                if (err) {
+                                    console.error(`Error moving file: ${err}`);
+                                    reject(err);
+                                    return;
+                                }
+                                console.log(`Moved input file to ${newLocation}`);
+                                resolve();
+                            });
+                        }
+                    })
+                    .on("error", function (err) {
+                        console.log("error: ", err);
+                        reject(err);
+                    })
+                    .run();
+            }
+        });
+    });
 };
 
 const inputDir = path.join(__dirname, "../input/");
 const outputPath = path.join(__dirname, "../output/");
 const chunkSize = 100 * 1024 * 1024; // 100MB
 
-// Find all .wmv files in the input directory
 const files = fs.readdirSync(inputDir).filter((file) => file.endsWith(".wmv"));
-// If no .wmv files found in the input directory, warn and end the script
 if (files.length === 0) {
-	console.warn(
-		"No .wmv files found in the input directory. Terminating script."
-	);
-	process.exit(0);
+    console.warn(
+        "No .wmv files found in the input directory. Terminating script."
+    );
+    process.exit(0);
 }
-// Start timer
 const startTime = Date.now();
 
-// Process each file
-files.forEach((file) => {
-	const inputPath = path.join(inputDir, file);
-	splitWMV(inputPath, outputPath, chunkSize);
+const processingTasks = files.map((file) => {
+    const inputPath = path.join(inputDir, file);
+    return splitWMV(inputPath, outputPath, chunkSize);
 });
 
-// End the timer and log the duration
-const endTime = Date.now();
-const elapsedTime = (endTime - startTime) / 1000;
-console.log(`Processing completed in ${elapsedTime} seconds.`);
+Promise.all(processingTasks).then(() => {
+    const endTime = Date.now();
+    const elapsedTime = (endTime - startTime) / 1000;
+    console.log(`Processing completed in ${elapsedTime.toFixed(2)} seconds.`);
+}).catch((error) => {
+    console.error("Error during process: ", error);
+});
